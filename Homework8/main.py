295a77deafe32885e62ca762c050697b8c712b82
Yaroslava Segal <yaroslavasiehal@gmail.com>
2023-12-26 23:31:16 +0200

    v2

--- Homework8/main.py
+++ Homework8/main.py
@@ -1,54 +1,58 @@
 from datetime import date, datetime, timedelta
 
+
 current_date = date.today()
 current_year = current_date.year
 
 
 def get_birthdays_per_week(users):
 
-    if users == []:
-        #якщо у списку немає користувачів, повертаємо пустий словник
+    if not users:
+        # if no user in users, we return empty dict
         return dict()
     else:
-        current_date = date.today()
-        current_year = current_date.year
+        today_date = date.today()
+        my_year = today_date.year
 
-        celebrate = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': []}
+        celebrate = {'Monday': [], 'Tuesday': [], 'Wednesday': [],
+                     'Thursday': [], 'Friday': []}
         for user in users:
-            user_date = user['birthday'].replace(year=current_year)
-            # переверіємо всі дні народження в цьому році
-            if timedelta(days=0) <= (user_date - current_date) <= timedelta(days=7):
-                # якщо день народження співпадає з тестовим днем або більше в межах тижня
-                if user_date.weekday() == 0 or user_date.weekday() == 5 or user_date.weekday() == 6:
-                    # якщо це понеділок, субота або неділя, додаємо користувача до списку по ключу 'Monday'
+            user_date = user['birthday'].replace(year=my_year)
+            # check all birthdays in this year
+            delta = user_date - today_date
+            if timedelta(days=0) <= delta <= timedelta(days=7):
+                # if birthday in on week with my day
+                day = user_date.weekday()
+                if day == 0 or day == 5 or day == 6:
+                    # if birthday is Monday, Saturday or Sunday
                     celebrate['Monday'].append(user['name'])
-                elif user_date.weekday() == 1:
-                    # якщо це вівторок, додаємо користувача до списку по ключу 'Tuesday'
+                elif day == 1:
                     celebrate['Tuesday'].append(user['name'])
-                elif user_date.weekday() == 2:
+                elif day == 2:
                     celebrate['Wednesday'].append(user['name'])
-                elif user_date.weekday() == 3:
+                elif day == 3:
                     celebrate['Thursday'].append(user['name'])
-                elif user_date.weekday() == 4:
+                elif day == 4:
                     celebrate['Friday'].append(user['name'])
 
             user1_date = user['birthday'].replace(year=current_year + 1)
-            # переверіємо всі дні народження в наступному році
-            if timedelta(days=0) <= (user1_date - current_date) <= timedelta(days=7):
-                if user1_date.weekday() == 0 or user1_date.weekday() == 5 or user1_date.weekday() == 6:
+            # check all birthdays in next year
+            delta1 = user1_date - current_date
+            if timedelta(days=0) <= delta1 <= timedelta(days=7):
+                day = user1_date.weekday()
+                if day == 0 or day == 5 or day == 6:
                     celebrate['Monday'].append(user['name'])
-                elif user1_date.weekday() == 1:
+                elif day == 1:
                     celebrate['Tuesday'].append(user['name'])
-                elif user1_date.weekday() == 2:
+                elif day == 2:
                     celebrate['Wednesday'].append(user['name'])
-                elif user1_date.weekday() == 3:
+                elif day == 3:
                     celebrate['Thursday'].append(user['name'])
-                elif user1_date.weekday() == 4:
+                elif day == 4:
                     celebrate['Friday'].append(user['name'])
         users = dict()
-        # створюємо словник іменинників тижня
         for key, value in celebrate.items():
-            if value != []:
+            if value:
                 users[key] = celebrate[key]
 
         return users
@@ -60,7 +64,6 @@ if __name__ == "__main__":
     ]
     result = get_birthdays_per_week(users)
     print(result)
-    # Виводимо результат
+
     for day_name, names in result.items():
         print(f"{day_name}: {', '.join(names)}")
-        # виводимо іменинників по дням тижня
